499145a6cd54aff519191e1cf965f1679e008456
c-rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-08-10 00:18:12 -0700

    Basic Jest setup

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types';
+
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  roots: ["tests/"],
+};
+
+export default config;
---
+++ tests/unit/emotes.manager.test.ts
@@ -0,0 +1,5 @@
+describe('Describe test', () => {
+  test('sample test', () => {
+
+  });
+});
